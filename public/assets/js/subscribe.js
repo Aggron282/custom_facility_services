b5fb79f4ba6c8df3b6e7df68e99adbfe8e1be078
Aggron282 <marcokhodr1@gmail.com>
2024-10-25 22:36:35 -0700

    S

--- public/assets/js/subscribe.js
+++
@@ -1,5 +0,0 @@
-var subscribe_form = document.querySelector("#subscribe");
-var subscribe_button = document.querySelector(".subscribe_button");
-
-
-subscribe_button.addEventListener()
--- routes/user/user_routes.js
+++ routes/user/user_routes.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 var userController = require("./../../controllers/user/userController.js")
+var adminController = require("./../../controllers/user/adminController.js")
+
 var rootDir = require("./../../util/path.js")
 
 router.get("/",userController.GetHomePage);
@@ -9,7 +11,7 @@ router.get("/about",userController.GetAboutUsPage);
 router.get("/schedule",userController.GetSchedulePage);
 router.get("/contact_us",userController.GetContactUsPage);
 router.get("/data/steps",userController.GetSteps);
-
+router.post("/admin/subscribe",adminController.Subscribe);
 router.post("/exit",userController.ExitOutOfModal);
 
 module.exports = router;
